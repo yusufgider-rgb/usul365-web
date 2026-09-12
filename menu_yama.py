9757ae0132764fbb206331b2de785753275729a2
Yusuf Gider <yusufgider@gmail.com>
2026-09-12 21:26:23 +0300

    temizlik: biten yama betikleri ve mukerrer uretici depodan cikarildi

--- menu_yama.py
+++
@@ -1,7 +0,0 @@
-import io
-y = "ortak.js"
-t = io.open(y, encoding="utf-8").read()
-t = t.replace('["Oyunlar",     "/#oyunlar"],',
-              '["Oyunlar",     "/#oyunlar"],\n    ["Playables",   "/#playables"],')
-io.open(y, "w", encoding="utf-8").write(t)
-print("menuye Playables eklendi")
